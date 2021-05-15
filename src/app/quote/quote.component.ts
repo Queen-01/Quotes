952acb3e2995c9b6eac978dbb99dfd244fa6eb4f
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-05-15 23:16:07 +0300

    add new quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote }from  '../quote'; 
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[]=[
+    new Quote(1, 'Dont stop when you are tired.Stop when you are done.', 'Wesley Snipes', new Date(2015, 7, 6)),
+  ]
 
   constructor() { }
 
