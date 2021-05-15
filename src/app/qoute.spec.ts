0b48c81e2fb07fd4375b6a98382e541a02742c19
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-05-16 00:50:08 +0300

    add changes

--- src/app/qoute.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Qoute } from './qoute';
-
-describe('Qoute', () => {
-  it('should create an instance', () => {
-    expect(new Qoute()).toBeTruthy();
-  });
-});
---
+++ src/app/quote.spec.ts
@@ -0,0 +1,8 @@
+import { Quote } from './quote';
+import { QuoteComponent } from './quote/quote.component';
+
+describe('Quote', () => {
+  it('should create an instance', () => {
+    expect(new QuoteComponent()).toBeTruthy();
+  });
+});
