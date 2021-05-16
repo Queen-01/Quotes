8c5e68ee0fd4710f57c851a5f2d4f268a616e0a7
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-05-16 16:28:55 +0300

    add changes made

---
+++ src/app/quote-detail/quote-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'
+
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
+  @Input() quote: Quote | undefined;
+    @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete)
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
